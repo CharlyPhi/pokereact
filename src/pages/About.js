c0420fd4eb4900fc939739c3cc9a137dd441627c
charly philippe <charly.ph@hotmail.fr>
2022-11-29 14:07:42 +0100

    a new commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,49 +1,114 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import { useState } from "react";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import useGeoLocation from "react-ipgeolocation";
+import Votes from "../components/Votes";
+
 
 const About = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [mail, setMail] = useState("");
+  const [msg, setMsg] = useState("");
+  const [error, setError] = useState("");
+  // get Ip country through "react-ipgeolocation";
+  const location = useGeoLocation();
+
+  document.body.style.zoom = "110%";
+  document.body.style.backgroundColor = "rgb(166,231,242,255)";
+
+  // Handles the Form Submit (only the errors at the moment, no actual submit)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      if (!firstName) {
+        setError("You need to enter a first name");
+      }
+      if (!lastName) {
+        setError("You need to enter a last name");
+      }
+      if (!mail) {
+        setError("You need to enter an email");
+      }
+      if (!msg) {
+        setError("Well there is no message to read here");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <Navigation/>
-      <h2>A propos</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-        voluptates dicta harum ad sed, incidunt dolor, autem quae mollitia
-        accusamus cum accusantium rerum rem sint pariatur veniam possimus
-        assumenda odit! Tempore omnis nostrum alias quo, nulla corporis esse
-        pariatur repellendus earum iure? Blanditiis possimus voluptates officia
-        optio maxime quisquam maiores nihil quam explicabo sint earum
-        laudantium, voluptatibus dicta sed vel quia corrupti perferendis unde
-        at! Odio quidem sapiente necessitatibus quaerat! Aspernatur ducimus
-        maiores maxime. Amet exercitationem vel suscipit molestiae nostrum
-        deleniti laboriosam qui dolore, cupiditate debitis, inventore modi
-        ratione, quia eaque nemo quam cum tempore illo eos facilis corporis
-        libero? Labore, exercitationem dolor suscipit sequi iusto vel, ipsum,
-        rerum voluptates quod distinctio reiciendis optio mollitia ad eius
-        eveniet doloremque perferendis alias omnis quisquam ratione magni
-        laboriosam aliquid. Doloremque fugit vel dolorem earum harum labore
-        cupiditate fuga sint iure voluptates. Odio, quibusdam laborum? Alias, ut
-        voluptatibus. Est, ipsa voluptas quisquam voluptates vel, dolor
-        assumenda odit labore hic sint eum atque reiciendis, illo possimus
-        voluptatem minima reprehenderit! Deleniti veniam, accusamus enim ea,
-        dolorem exercitationem distinctio minus eveniet tenetur quaerat
-        reiciendis necessitatibus, voluptate eum asperiores cupiditate
-        aspernatur. Iusto repudiandae cum maxime reprehenderit minima magnam
-        quaerat asperiores repellat necessitatibus officia totam est sint fugiat
-        tenetur, beatae, optio illum quo nostrum saepe sapiente ipsa iste
-        dolores corrupti. Cupiditate a libero ut nostrum vel suscipit recusandae
-        laudantium, eligendi voluptate vero architecto quis maxime, ullam
-        voluptas voluptatibus eveniet nam accusamus labore, possimus tenetur
-        natus explicabo harum reprehenderit? Error consequuntur repudiandae
-        libero autem nostrum ad, rem, vero explicabo possimus sed animi?
-        Asperiores a architecto molestias sit quibusdam earum ullam. Delectus
-        consectetur enim labore corporis. Nam quasi sint eius molestias quia ut.
-        Harum soluta odit quia aspernatur illo incidunt explicabo id doloribus,
-        eveniet fuga, dicta odio omnis sit ipsum fugit expedita maiores
-        veritatis itaque, voluptates error necessitatibus nobis ipsam est?
-        Deserunt minus nobis obcaecati fugit quae corrupti exercitationem minima
-        iure provident!
+      <Navigation />
+      <br></br>
+      <h1>A propos</h1>
+      <div className="about-top">
+        <div className="about-top-left">
+      <p className="about-top-text">
+        This Website's current aim is to improve my React skills, it will most
+        likely only be visited by unimpressed recruiters and family members. Use the vote below to help me choose the next feature to implement. If you have any input regarding what could be improved use the form below and send me an email !
       </p>
+      <div className="vote">
+        <Votes/>
+      </div>
+      </div>
+      <img className = "pifacteur" src="mail_pikachu.jpg" alt="Pikachu_mail"></img>
+      </div>
+
+      <section className="form">
+        <div className="pikachu"></div>
+        <p>Contact Us</p>
+        <div className="contact-form">
+          <label htmlFor="First name">First name</label>
+          <input
+            id="First name"
+            placeholder="First name"
+            required={true}
+            onChange={(e) => setFirstName(e.target.value)}
+          ></input>
+          <label htmlFor="Last name">Last name</label>
+          <input
+            id="Last name"
+            placeholder="Last name"
+            required={true}
+            onChange={(e) => setLastName(e.target.value)}
+          ></input>
+          <label htmlFor="Mobile">Mobile</label>
+          <PhoneInput
+            id="Mobile"
+            international
+            countryCallingCodeEditable={false}
+            placeholder="Enter phone number"
+            defaultCountry={`${location.country}`}
+            defaultValue="+33"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <label htmlFor="e-mail">E-mail</label>
+          <input
+            type="email"
+            id="E-mail"
+            placeholder="E-mail"
+            required={true}
+            onChange={(e) => setMail(e.target.value)}
+          ></input>
+          <label htmlFor="Message">Message</label>
+          <textarea
+            id="Message"
+            placeholder="Message"
+            minLength={10}
+            maxLength={200}
+            required={true}
+            onChange={(e) => setMsg(e.target.value)}
+          ></textarea>
+          <input type="submit" onClick={handleSubmit} />
+          <p>{error}</p>
+        </div>
+      </section>
     </div>
   );
 };
--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,26 +1,15 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import Comments from "../components/Comments/Comments"
+
+document.body.style.zoom = "110%";
+document.body.style.backgroundColor = "rgb(166,231,242,255)";
 
 const Faq = () => {
   return (
     <div className="faq">
       <Navigation />
-      <h2>Which is your favorite Pokemon ?</h2>
-      <h3>
-        I dont really have one, I mean Mewtwo is pretty rad but man... Psyduck !
-      </h3>
-      <h2>Why did you create this site ?</h2>
-      <h3>To learn Javascript and more precisely React.</h3>
-      <h2>How was your day ?</h2>
-      <h3>Quite good actually, seen some friends, pet my dog.</h3>
-      <h2>Do you play Pokemon Go ?</h2>
-      <h3>I do indeed, and have been since the game started pretty much.</h3>
-      <h2>What is your favorite color ?</h2>
-      <h3>Some shade of red.</h3>
-      <h2>Do you think the human race has a future somehow ?</h2>
-      <h3>
-        Not really, we were doomed to destroy ourselves from the beginning.
-      </h3>
+      <Comments currentUserId="1"/>
     </div>
   );
 };
