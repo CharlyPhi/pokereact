9c94a1abda4e614411bee1e5692ee86da5177d5a
charly philippe <charly.ph@hotmail.fr>
2023-01-12 16:37:43 +0100

    Dashboard added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import {React, useState} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Faq from "./pages/Faq";
 import Homepage from "./pages/Homepage";
 import Pokedex from "./pages/Pokedex";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
-  const [loggedIn, setLoggedIn]
+  const [loggedIn, setLoggedIn] = useState("Not logged in");
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/Homepage" element={<Homepage />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="Pokedex" element={<Pokedex />} />
         <Route path="/about" element={<About />} />
-        <Route path="/Homepage" element={<Homepage />} />
         <Route path="*" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
